ea811f6b8de3b2333595ca2476fd3a75990c7ada
Charles Beam <charleslbeam@gmail.com>
2024-03-25 12:35:51 -0500

    updated pdf embed

--- src/components/Constitution.tsx
+++ src/components/Constitution.tsx
@@ -1,10 +1,10 @@
 const Constitution = () => {
   return (
     <div className="relative w-full h-full justify-center items-center">
-      <iframe
+      <embed
         src="https://pub-4a5c351e48c34a18817678ead6714bda.r2.dev/EDCLSU%20Constitution.pdf"
         className="w-full min-h-screen"
-        title="EDC Constitution"
+        type="application/pdf"
       />
     </div>
   );
